b80a249150f842172ef890ca855c16b83c25ee24
Vishwjit Upase <vishwjitupase2807@gmail.com>
2026-03-18 23:33:14 +0530

    Time: 0 ms (100.00%) | Memory: 57.8 MB (78.99%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -0,0 +1,11 @@
+var getConcatenation = function(nums) {
+    let n = nums.length;
+    let ans = new Array(2 * n);
+
+    for (let i = 0; i < n; i++) {
+        ans[i] = nums[i];
+        ans[i + n] = nums[i];
+    }
+
+    return ans;
+};
